c24dd7201a3a63159ceda281180d8c1787619566
albertoKanroji <albert.hdz855@gmail.com>
2024-05-06 16:58:40 -0600

    vercel analyticcs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { inject } from '@vercel/analytics';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -8,7 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-inject();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
